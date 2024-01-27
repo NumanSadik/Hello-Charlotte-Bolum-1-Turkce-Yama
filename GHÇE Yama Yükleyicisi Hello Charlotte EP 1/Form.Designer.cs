b1d2028084aef6c1b21f68160f7a97a9f91874fc
Numan Sadık Şen <68291382+NumanSadik@users.noreply.github.com>
2024-01-27 18:44:58 +0300

    adsgfdhgdh

--- GHÇE Yama Yükleyicisi Hello Charlotte EP 1/Form.Designer.cs
+++ GHÇE Yama Yükleyicisi Hello Charlotte EP 1/Form.Designer.cs
@@ -32,28 +32,32 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
             Step0 = new Panel();
-            PreviewOfTheGame = new PictureBox();
-            TranslationInformation = new RichTextBox();
-            Buttons = new Panel();
+            Step1 = new Panel();
+            FileExamplePicture = new PictureBox();
+            richTextBox1 = new RichTextBox();
             FileExplorerPanel = new Panel();
             FileExplorerButton = new Button();
             FileExplorerPath = new RichTextBox();
-            richTextBox1 = new RichTextBox();
-            Step1 = new Panel();
-            FileExamplePicture = new PictureBox();
+            PreviewOfTheGame = new PictureBox();
+            TranslationInformation = new RichTextBox();
+            Buttons = new Panel();
+            Step2 = new Panel();
+            ProgressLog = new RichTextBox();
             PrevBtn = new Button();
             NextBtn = new Button();
             Step0.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)PreviewOfTheGame).BeginInit();
-            Buttons.SuspendLayout();
-            FileExplorerPanel.SuspendLayout();
             Step1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)FileExamplePicture).BeginInit();
+            FileExplorerPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PreviewOfTheGame).BeginInit();
+            Buttons.SuspendLayout();
+            Step2.SuspendLayout();
             SuspendLayout();
             // 
             // Step0
             // 
             Step0.BackColor = SystemColors.ControlLight;
+            Step0.Controls.Add(Step1);
             Step0.Controls.Add(PreviewOfTheGame);
             Step0.Controls.Add(TranslationInformation);
             Step0.Location = new Point(12, 12);
@@ -61,55 +65,39 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
             Step0.Size = new Size(460, 394);
             Step0.TabIndex = 0;
             // 
-            // PreviewOfTheGame
+            // Step1
             // 
-            PreviewOfTheGame.BorderStyle = BorderStyle.Fixed3D;
-            PreviewOfTheGame.Image = Properties.Resources.hello_charlotte_orig;
-            PreviewOfTheGame.Location = new Point(0, 0);
-            PreviewOfTheGame.Name = "PreviewOfTheGame";
-            PreviewOfTheGame.Size = new Size(188, 394);
-            PreviewOfTheGame.SizeMode = PictureBoxSizeMode.CenterImage;
-            PreviewOfTheGame.TabIndex = 1;
-            PreviewOfTheGame.TabStop = false;
+            Step1.Controls.Add(FileExamplePicture);
+            Step1.Controls.Add(richTextBox1);
+            Step1.Controls.Add(FileExplorerPanel);
+            Step1.Location = new Point(0, 0);
+            Step1.Name = "Step1";
+            Step1.Size = new Size(460, 394);
+            Step1.TabIndex = 2;
+            Step1.Visible = false;
             // 
-            // TranslationInformation
+            // FileExamplePicture
             // 
-            TranslationInformation.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            TranslationInformation.Location = new Point(194, 0);
-            TranslationInformation.Name = "TranslationInformation";
-            TranslationInformation.Size = new Size(266, 394);
-            TranslationInformation.TabIndex = 0;
-            TranslationInformation.Text = resources.GetString("TranslationInformation.Text");
+            FileExamplePicture.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            FileExamplePicture.BackgroundImage = Properties.Resources.Ekran_görüntüsü_2024_01_25_194655;
+            FileExamplePicture.BackgroundImageLayout = ImageLayout.Zoom;
+            FileExamplePicture.BorderStyle = BorderStyle.Fixed3D;
+            FileExamplePicture.ErrorImage = null;
+            FileExamplePicture.InitialImage = null;
+            FileExamplePicture.Location = new Point(3, 255);
+            FileExamplePicture.Name = "FileExamplePicture";
+            FileExamplePicture.Size = new Size(454, 120);
+            FileExamplePicture.TabIndex = 2;
+            FileExamplePicture.TabStop = false;
             // 
-            // Buttons
+            // richTextBox1
             // 
-            Buttons.BackColor = SystemColors.ControlLight;
-            Buttons.Controls.Add(PrevBtn);
-            Buttons.Controls.Add(NextBtn);
-            Buttons.Location = new Point(290, 412);
-            Buttons.Name = "Buttons";
-            Buttons.Size = new Size(182, 37);
-            Buttons.TabIndex = 1;
-            // 
-            // PrevBtn
-            // 
-            PrevBtn.Location = new Point(3, 3);
-            PrevBtn.Name = "PrevBtn";
-            PrevBtn.Size = new Size(85, 30);
-            PrevBtn.TabIndex = 1;
-            PrevBtn.Text = "Önceki";
-            PrevBtn.UseVisualStyleBackColor = true;
-            PrevBtn.Click += PrevBtn_Click;
-            // 
-            // NextBtn
-            // 
-            NextBtn.Location = new Point(94, 3);
-            NextBtn.Name = "NextBtn";
-            NextBtn.Size = new Size(85, 30);
-            NextBtn.TabIndex = 0;
-            NextBtn.Text = "Sonraki";
-            NextBtn.UseVisualStyleBackColor = true;
-            NextBtn.Click += NextBtn_Click;
+            richTextBox1.Location = new Point(3, 3);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(454, 203);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "Steam üzerinden yüklenmiş olan Hello Charlotte EP1 oyununun ana dizinindeki Game.rgss3a dosyasını seçiniz.\n\nÖrnek yol:\n\"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Hello Charlotte\\Game.rgss3a\"\n";
             // 
             // FileExplorerPanel
             // 
@@ -145,39 +133,74 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
             FileExplorerPath.TabIndex = 0;
             FileExplorerPath.Text = "";
             // 
-            // richTextBox1
+            // PreviewOfTheGame
             // 
-            richTextBox1.Location = new Point(3, 3);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new Size(454, 203);
-            richTextBox1.TabIndex = 1;
-            richTextBox1.Text = "Steam üzerinden yüklenmiş olan Hello Charlotte EP1 oyununun ana dizinindeki Game.rgss3a dosyasını seçiniz.\n\nÖrnek yol:\n\"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Hello Charlotte\\Game.rgss3a\"\n";
+            PreviewOfTheGame.BorderStyle = BorderStyle.Fixed3D;
+            PreviewOfTheGame.Image = Properties.Resources.hello_charlotte_orig;
+            PreviewOfTheGame.Location = new Point(0, 0);
+            PreviewOfTheGame.Name = "PreviewOfTheGame";
+            PreviewOfTheGame.Size = new Size(188, 394);
+            PreviewOfTheGame.SizeMode = PictureBoxSizeMode.CenterImage;
+            PreviewOfTheGame.TabIndex = 1;
+            PreviewOfTheGame.TabStop = false;
             // 
-            // Step1
+            // TranslationInformation
             // 
-            Step1.Controls.Add(FileExamplePicture);
-            Step1.Controls.Add(richTextBox1);
-            Step1.Controls.Add(FileExplorerPanel);
-            Step1.Location = new Point(12, 12);
-            Step1.Name = "Step1";
-            Step1.Size = new Size(460, 394);
-            Step1.TabIndex = 2;
-            Step1.Visible = false;
+            TranslationInformation.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            TranslationInformation.Location = new Point(194, 0);
+            TranslationInformation.Name = "TranslationInformation";
+            TranslationInformation.ReadOnly = true;
+            TranslationInformation.Size = new Size(266, 394);
+            TranslationInformation.TabIndex = 0;
+            TranslationInformation.Text = resources.GetString("TranslationInformation.Text");
             // 
-            // FileExamplePicture
+            // Buttons
             // 
-            FileExamplePicture.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            FileExamplePicture.BackgroundImage = Properties.Resources.Ekran_görüntüsü_2024_01_25_194655;
-            FileExamplePicture.BackgroundImageLayout = ImageLayout.Zoom;
-            FileExamplePicture.BorderStyle = BorderStyle.Fixed3D;
-            FileExamplePicture.ErrorImage = null;
-            FileExamplePicture.InitialImage = null;
-            FileExamplePicture.Location = new Point(3, 255);
-            FileExamplePicture.Name = "FileExamplePicture";
-            FileExamplePicture.Size = new Size(454, 120);
-            FileExamplePicture.TabIndex = 2;
-            FileExamplePicture.TabStop = false;
+            Buttons.BackColor = SystemColors.ControlLight;
+            Buttons.Controls.Add(PrevBtn);
+            Buttons.Controls.Add(NextBtn);
+            Buttons.Location = new Point(290, 412);
+            Buttons.Name = "Buttons";
+            Buttons.Size = new Size(182, 37);
+            Buttons.TabIndex = 1;
+            // 
+            // PrevBtn
+            // 
+            PrevBtn.Location = new Point(3, 3);
+            PrevBtn.Name = "PrevBtn";
+            PrevBtn.Size = new Size(85, 30);
+            PrevBtn.TabIndex = 1;
+            PrevBtn.Text = "Önceki";
+            PrevBtn.UseVisualStyleBackColor = true;
+            PrevBtn.Click += PrevBtn_Click;
+            // 
+            // NextBtn
+            // 
+            NextBtn.Location = new Point(94, 3);
+            NextBtn.Name = "NextBtn";
+            NextBtn.Size = new Size(85, 30);
+            NextBtn.TabIndex = 0;
+            NextBtn.Text = "Sonraki";
+            NextBtn.UseVisualStyleBackColor = true;
+            NextBtn.Click += NextBtn_Click;
+            // 
+            // Step2
+            // 
+            Step2.Controls.Add(ProgressLog);
+            Step2.Location = new Point(9, 12);
+            Step2.Name = "Step2";
+            Step2.Size = new Size(460, 394);
+            Step2.TabIndex = 3;
+            Step2.Visible = false;
+            // 
+            // ProgressLog
+            // 
+            ProgressLog.Location = new Point(3, 3);
+            ProgressLog.Name = "ProgressLog";
+            ProgressLog.ReadOnly = true;
+            ProgressLog.Size = new Size(454, 388);
+            ProgressLog.TabIndex = 0;
+            ProgressLog.Text = "";
             // 
             // Window
             // 
@@ -185,7 +208,7 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             ClientSize = new Size(484, 461);
-            Controls.Add(Step1);
+            Controls.Add(Step2);
             Controls.Add(Buttons);
             Controls.Add(Step0);
             Icon = (Icon)resources.GetObject("$this.Icon");
@@ -197,11 +220,12 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
             Text = "Game Hunters Çeviri Ekibi Yama Yükleyicisi";
             Load += Window_Load;
             Step0.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)PreviewOfTheGame).EndInit();
-            Buttons.ResumeLayout(false);
-            FileExplorerPanel.ResumeLayout(false);
             Step1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)FileExamplePicture).EndInit();
+            FileExplorerPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)PreviewOfTheGame).EndInit();
+            Buttons.ResumeLayout(false);
+            Step2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -209,6 +233,7 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
 
         public Panel Step0;
         public Panel Step1;
+        public Panel Step2;
         private Panel Buttons;
         public Button NextBtn;
         public Button PrevBtn;
@@ -216,8 +241,9 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
         private PictureBox PreviewOfTheGame;
         private Panel FileExplorerPanel;
         private Button FileExplorerButton;
-        private RichTextBox FileExplorerPath;
+        public RichTextBox FileExplorerPath;
         private RichTextBox richTextBox1;
         private PictureBox FileExamplePicture;
+        public RichTextBox ProgressLog;
     }
 }
--- GHÇE Yama Yükleyicisi Hello Charlotte EP 1/Form.cs
+++ GHÇE Yama Yükleyicisi Hello Charlotte EP 1/Form.cs
@@ -13,6 +13,18 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
             InitializeComponent();
             steps.Add(Step0);
             steps.Add(Step1);
+            steps.Add(Step2);
+        }
+
+        private void PrevBtn_Click(object sender, EventArgs e)
+        {
+            currentStep--;
+            steps[currentStep].Visible = false;
+            if (currentStep == 0)
+            {
+                PrevBtn.Enabled = false;
+            }
+            NextBtn.Enabled = true;
         }
 
         private void NextBtn_Click(object sender, EventArgs e)
@@ -24,6 +36,27 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
                 NextBtn.Enabled = false;
             }
             PrevBtn.Enabled = true;
+
+            if (currentStep == 1)
+            {
+                NextBtn.Enabled = false;
+            }
+
+            if (currentStep == 2)
+            {
+                /*using (Process pProcess = new Process())
+                {
+                    pProcess.StartInfo.FileName = @ "RPGMakerDecrypter-cli";
+                    pProcess.StartInfo.Arguments = FileExplorerPath.Text; //argument
+                    pProcess.StartInfo.UseShellExecute = false;
+                    pProcess.StartInfo.RedirectStandardOutput = true;
+                    //pProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    //pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                    pProcess.Start();
+                    ProgressLog.Text = pProcess.StandardOutput.ReadToEnd(); //The output result
+                    pProcess.WaitForExit();
+                }*/
+            }
         }
 
         private void FileExplorerButton_Click(object sender, EventArgs e)
@@ -41,20 +74,10 @@ namespace GHÇE_Yama_Yükleyicisi_Hello_Charlotte_EP_1
                 {
                     //Get the path of specified file
                     filePath = openFileDialog.FileName;
+                    NextBtn.Enabled = true;
                 }
             }
             FileExplorerPath.Text = filePath;
-        }
-
-        private void PrevBtn_Click(object sender, EventArgs e)
-        {
-            currentStep--;
-            steps[currentStep].Visible = false;
-            if (currentStep == 0)
-            {
-                PrevBtn.Enabled = false;
-            }
-            NextBtn.Enabled = true;
         }
 
         private void Window_Load(object sender, EventArgs e)
