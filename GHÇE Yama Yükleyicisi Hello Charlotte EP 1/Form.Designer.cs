0589a65a215865c30f2d894353a8579aeff26a04
Numan Sadık Şen <68291382+NumanSadik@users.noreply.github.com>
2025-02-06 02:24:59 +0300

    bluesoul görünüm

--- GHÇE Yama Yükleyicisi Hello Charlotte EP 1/Form.Designer.cs
+++ GHÇE Yama Yükleyicisi Hello Charlotte EP 1/Form.Designer.cs
@@ -63,9 +63,10 @@
             Step0.Controls.Add(Step1);
             Step0.Controls.Add(PreviewOfTheGame);
             Step0.Controls.Add(TranslationInformation);
-            Step0.Location = new Point(12, 12);
+            Step0.Location = new Point(14, 16);
+            Step0.Margin = new Padding(3, 4, 3, 4);
             Step0.Name = "Step0";
-            Step0.Size = new Size(460, 394);
+            Step0.Size = new Size(526, 525);
             Step0.TabIndex = 0;
             // 
             // Step1
@@ -74,8 +75,9 @@
             Step1.Controls.Add(richTextBox1);
             Step1.Controls.Add(FileExplorerPanel);
             Step1.Location = new Point(0, 0);
+            Step1.Margin = new Padding(3, 4, 3, 4);
             Step1.Name = "Step1";
-            Step1.Size = new Size(460, 394);
+            Step1.Size = new Size(526, 525);
             Step1.TabIndex = 2;
             Step1.Visible = false;
             // 
@@ -87,18 +89,20 @@
             FileExamplePicture.BorderStyle = BorderStyle.Fixed3D;
             FileExamplePicture.ErrorImage = null;
             FileExamplePicture.InitialImage = null;
-            FileExamplePicture.Location = new Point(3, 255);
+            FileExamplePicture.Location = new Point(3, 340);
+            FileExamplePicture.Margin = new Padding(3, 4, 3, 4);
             FileExamplePicture.Name = "FileExamplePicture";
-            FileExamplePicture.Size = new Size(454, 120);
+            FileExamplePicture.Size = new Size(518, 159);
             FileExamplePicture.TabIndex = 2;
             FileExamplePicture.TabStop = false;
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(3, 3);
+            richTextBox1.Location = new Point(3, 4);
+            richTextBox1.Margin = new Padding(3, 4, 3, 4);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new Size(454, 203);
+            richTextBox1.Size = new Size(518, 269);
             richTextBox1.TabIndex = 1;
             richTextBox1.Text = "Steam üzerinden yüklenmiş olan Hello Charlotte EP1 oyununun ana dizinindeki Game.rgss3a dosyasını seçiniz.\n\nÖrnek yol:\n\"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Hello Charlotte\\Game.rgss3a\"\n";
             // 
@@ -108,17 +112,19 @@
             FileExplorerPanel.BorderStyle = BorderStyle.Fixed3D;
             FileExplorerPanel.Controls.Add(FileExplorerButton);
             FileExplorerPanel.Controls.Add(FileExplorerPath);
-            FileExplorerPanel.Location = new Point(3, 212);
+            FileExplorerPanel.Location = new Point(3, 283);
+            FileExplorerPanel.Margin = new Padding(3, 4, 3, 4);
             FileExplorerPanel.Name = "FileExplorerPanel";
-            FileExplorerPanel.Size = new Size(454, 37);
+            FileExplorerPanel.Size = new Size(518, 48);
             FileExplorerPanel.TabIndex = 0;
             // 
             // FileExplorerButton
             // 
             FileExplorerButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            FileExplorerButton.Location = new Point(362, 3);
+            FileExplorerButton.Location = new Point(414, 4);
+            FileExplorerButton.Margin = new Padding(3, 4, 3, 4);
             FileExplorerButton.Name = "FileExplorerButton";
-            FileExplorerButton.Size = new Size(85, 30);
+            FileExplorerButton.Size = new Size(97, 40);
             FileExplorerButton.TabIndex = 1;
             FileExplorerButton.Text = "Seç";
             FileExplorerButton.UseVisualStyleBackColor = true;
@@ -128,11 +134,12 @@
             // 
             FileExplorerPath.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             FileExplorerPath.DetectUrls = false;
-            FileExplorerPath.Location = new Point(3, 3);
+            FileExplorerPath.Location = new Point(3, 4);
+            FileExplorerPath.Margin = new Padding(3, 4, 3, 4);
             FileExplorerPath.Multiline = false;
             FileExplorerPath.Name = "FileExplorerPath";
             FileExplorerPath.ScrollBars = RichTextBoxScrollBars.None;
-            FileExplorerPath.Size = new Size(356, 30);
+            FileExplorerPath.Size = new Size(406, 39);
             FileExplorerPath.TabIndex = 0;
             FileExplorerPath.Text = "";
             FileExplorerPath.TextChanged += FileExplorerPath_TextChanged;
@@ -142,8 +149,9 @@
             PreviewOfTheGame.BorderStyle = BorderStyle.Fixed3D;
             PreviewOfTheGame.Image = Properties.Resources.hello_charlotte_orig;
             PreviewOfTheGame.Location = new Point(0, 0);
+            PreviewOfTheGame.Margin = new Padding(3, 4, 3, 4);
             PreviewOfTheGame.Name = "PreviewOfTheGame";
-            PreviewOfTheGame.Size = new Size(188, 394);
+            PreviewOfTheGame.Size = new Size(214, 524);
             PreviewOfTheGame.SizeMode = PictureBoxSizeMode.CenterImage;
             PreviewOfTheGame.TabIndex = 1;
             PreviewOfTheGame.TabStop = false;
@@ -151,10 +159,11 @@
             // TranslationInformation
             // 
             TranslationInformation.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            TranslationInformation.Location = new Point(194, 0);
+            TranslationInformation.Location = new Point(222, 0);
+            TranslationInformation.Margin = new Padding(3, 4, 3, 4);
             TranslationInformation.Name = "TranslationInformation";
             TranslationInformation.ReadOnly = true;
-            TranslationInformation.Size = new Size(266, 394);
+            TranslationInformation.Size = new Size(303, 524);
             TranslationInformation.TabIndex = 0;
             TranslationInformation.Text = resources.GetString("TranslationInformation.Text");
             // 
@@ -163,16 +172,18 @@
             Buttons.BackColor = SystemColors.ControlLight;
             Buttons.Controls.Add(PrevBtn);
             Buttons.Controls.Add(NextBtn);
-            Buttons.Location = new Point(290, 412);
+            Buttons.Location = new Point(331, 549);
+            Buttons.Margin = new Padding(3, 4, 3, 4);
             Buttons.Name = "Buttons";
-            Buttons.Size = new Size(182, 37);
+            Buttons.Size = new Size(208, 49);
             Buttons.TabIndex = 1;
             // 
             // PrevBtn
             // 
-            PrevBtn.Location = new Point(3, 3);
+            PrevBtn.Location = new Point(3, 4);
+            PrevBtn.Margin = new Padding(3, 4, 3, 4);
             PrevBtn.Name = "PrevBtn";
-            PrevBtn.Size = new Size(85, 30);
+            PrevBtn.Size = new Size(97, 40);
             PrevBtn.TabIndex = 1;
             PrevBtn.Text = "Önceki";
             PrevBtn.UseVisualStyleBackColor = true;
@@ -180,9 +191,10 @@
             // 
             // NextBtn
             // 
-            NextBtn.Location = new Point(94, 3);
+            NextBtn.Location = new Point(107, 4);
+            NextBtn.Margin = new Padding(3, 4, 3, 4);
             NextBtn.Name = "NextBtn";
-            NextBtn.Size = new Size(85, 30);
+            NextBtn.Size = new Size(97, 40);
             NextBtn.TabIndex = 0;
             NextBtn.Text = "Sonraki";
             NextBtn.UseVisualStyleBackColor = true;
@@ -190,10 +202,11 @@
             // 
             // ProgressLog
             // 
-            ProgressLog.Location = new Point(3, 3);
+            ProgressLog.Location = new Point(3, 4);
+            ProgressLog.Margin = new Padding(3, 4, 3, 4);
             ProgressLog.Name = "ProgressLog";
             ProgressLog.ReadOnly = true;
-            ProgressLog.Size = new Size(454, 388);
+            ProgressLog.Size = new Size(518, 516);
             ProgressLog.TabIndex = 0;
             ProgressLog.Text = "";
             ProgressLog.WordWrap = false;
@@ -203,9 +216,10 @@
             // 
             Step2.Controls.Add(Step3);
             Step2.Controls.Add(ProgressLog);
-            Step2.Location = new Point(9, 12);
+            Step2.Location = new Point(10, 16);
+            Step2.Margin = new Padding(3, 4, 3, 4);
             Step2.Name = "Step2";
-            Step2.Size = new Size(460, 394);
+            Step2.Size = new Size(526, 525);
             Step2.TabIndex = 3;
             Step2.Visible = false;
             // 
@@ -215,8 +229,9 @@
             Step3.Controls.Add(pictureBox1);
             Step3.Controls.Add(richTextBox2);
             Step3.Location = new Point(0, 0);
+            Step3.Margin = new Padding(3, 4, 3, 4);
             Step3.Name = "Step3";
-            Step3.Size = new Size(460, 394);
+            Step3.Size = new Size(526, 525);
             Step3.TabIndex = 4;
             Step3.Visible = false;
             // 
@@ -224,9 +239,10 @@
             // 
             pictureBox1.BorderStyle = BorderStyle.Fixed3D;
             pictureBox1.Image = Properties.Resources.hello_charlotte_orig;
-            pictureBox1.Location = new Point(0, 3);
+            pictureBox1.Location = new Point(0, 4);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(188, 394);
+            pictureBox1.Size = new Size(214, 524);
             pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
@@ -234,29 +250,31 @@
             // richTextBox2
             // 
             richTextBox2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            richTextBox2.Location = new Point(194, 3);
+            richTextBox2.Location = new Point(222, 4);
+            richTextBox2.Margin = new Padding(3, 4, 3, 4);
             richTextBox2.Name = "richTextBox2";
             richTextBox2.ReadOnly = true;
-            richTextBox2.Size = new Size(266, 388);
+            richTextBox2.Size = new Size(303, 516);
             richTextBox2.TabIndex = 0;
             richTextBox2.Text = resources.GetString("richTextBox2.Text");
             // 
             // Window
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
-            ClientSize = new Size(484, 461);
+            ClientSize = new Size(551, 604);
             Controls.Add(Step2);
             Controls.Add(Buttons);
             Controls.Add(Step0);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
-            MaximumSize = new Size(500, 500);
-            MinimumSize = new Size(500, 500);
+            MaximumSize = new Size(569, 651);
+            MinimumSize = new Size(569, 651);
             Name = "Window";
             ShowIcon = false;
-            Text = "Game Hunters Çeviri Ekibi Yama Yükleyicisi";
+            Text = "Bluesoul Çeviri Ekibi Yama Yükleyicisi";
             Step0.ResumeLayout(false);
             Step1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)FileExamplePicture).EndInit();
